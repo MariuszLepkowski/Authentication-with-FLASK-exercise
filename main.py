1f2a93a5008ac416709428d0b740a6d96acd36c6
Mariusz <mariusz.lepkowski23@gmail.com>
2023-08-30 20:54:14 +0200

    Hide Login and Register buttons when the user is logged in.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sqlalchemy.exc
-from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory, get_flashed_messages
+from flask import Flask, render_template, request, url_for, redirect, send_from_directory, get_flashed_messages
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
